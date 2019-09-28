4421716d77b05ae69ce63c92fdb92912dd743fc1
Leonardo Silva <coelho_ll@hotmail.com>
2019-09-28 17:28:53 -0600

    changes

--- web/week02-assignment/javaScript.js
+++ web/week02-assignment/javaScript.js
@@ -1,21 +1,27 @@
 const nav = () => {
-	const burger = document.querySelector('.burger');
-	const navegation = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
-	
-	
-	
-	
-	burger.addEventListener('click',() => {
-		navegation.classList.toggle('nav-active');
-	});
+  const burger = document.querySelector(".burger");
+  const navegation = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-	navLinks.forEach((link, index) => {
+  burger.addEventListener("click", () => {
+    //my toggle
+    navegation.classList.toggle("nav-active");
 
-		link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-		
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          1.5}s`;
+      }
 	});
-}
+	
+	//my Burger Animation
+	burger.classList.toggle('toggle');
+
+  });
+};
+
 //const app = ()=> {
 nav();
-
