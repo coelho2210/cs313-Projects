8a55524223d6ccf195e98a1a3d66a84b0c3e6d96
Leonardo Silva <coelho_ll@hotmail.com>
2019-09-28 16:33:38 -0600

    changes

--- web/week02-assignment/javaScript.js
+++ web/week02-assignment/javaScript.js
@@ -1,17 +1,21 @@
-// wiltbank02.js
+const nav = () => {
+	const burger = document.querySelector('.burger');
+	const navegation = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+	
+	
+	
+	
+	burger.addEventListener('click',() => {
+		navegation.classList.toggle('nav-active');
+	});
 
-function clickAlert() {
-  alert("Clicked!");
+	navLinks.forEach((link, index) => {
+
+		link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+		
+	});
 }
-//following function blocked out while using jquery
-/*
-function changeColor() {
-	var textbox_id = "txtColor";
-	var textbox = document.getElementById(textbox_id);
-	var div_id = "div1";
-	var div = document.getElementById(div_id);
-	// We should verify values here before we use them...
-	var color = textbox.value;
-	div.style.backgroundColor = color;
-}
-*/
+//const app = ()=> {
+nav();
+
