9d213e255e5510a352fa4ada532e4e30224a8a17
Leonardo Silva <coelho_ll@hotmail.com>
2019-09-28 12:40:28 -0600

    change

--- web/week02-assignment/code.php
+++ web/week02-assignment/code.php
@@ -1,23 +1,23 @@
 <?php
-echo "<div>Test</div>";
-if(isset($_POST['submit'])){
+
+//if(isset($_POST['submit'])){
 
 
-$name = $_POST['name'];
-$subject = $_POST['subject'];
-$mailFrom = $_POST['email'];
-$message = $_POST['message'];
-
+//$name = $_POST['name'];
+//$subject = $_POST['subject'];
+//$mailFrom = $_POST['email'];
+//$message = $_POST['message'];
+$subject = "OI vem semore aqui?";
 
 $emailTo = "coelho_ll@hotmail.com";
-$headers = "From: ".mailFrom;
-
-$txt = "You have received an email from ".$name.".\n\n".$message;
-
+//$headers = "From: ".mailFrom;
+$headers = "From: sil18001@byi.edu";
+//$txt = "You have received an email from ".$name.".\n\n".$message;
+$txt = "You have received an email from  leo";
 
 mail($emailTo,$subject,$txt,$headers);
 header("Location: contact.html?emailsend");
 
-}
+//}
 
 ?>
