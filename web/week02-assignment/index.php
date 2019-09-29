aa3e0c12e7b3749e52fe04c3c8570a9fff1702d1
Leonardo Silva <coelho_ll@hotmail.com>
2019-09-28 21:57:49 -0600

    changes

--- web/week02-assignment/index.php
+++ web/week02-assignment/index.php
@@ -14,22 +14,8 @@
 
 <body>
   <nav>
-    <div class="logo">
-      <h4>LC</h4>
-    </div>
-    <ul class="nav-links">
-      <li><a href="assignment.html">Projects</a></li>
-      <li><a href="index.html">Homepage</a></li>
-      <li><a href="contact.html">Contact</a></li>
-      <li><a href="aboutMe.html">About</a></li>
-    </ul>
-    <div class="burger">
-
-      <div class="l1"></div>
-      <div class="l2"></div>
-      <div class="l3"></div>
-
-    </div>
+   <?php include('navigation/navigation.php'); ?>
+   
   </nav>
   <div class="introduction">
     <img src="image/me.jpg" alt="" class="box-img">
