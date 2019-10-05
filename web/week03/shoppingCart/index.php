05a6053cd044231f496e353c8f52eafec8a4eef6
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-04 19:22:05 -0600

    myShoppingCart

--- web/week03/shoppingCart/index.php
+++ web/week03/shoppingCart/index.php
@@ -1,76 +1,78 @@
 <?php
 session_start();
-$product_array = array( array( 'id' => "1", 'name' => "Soccer-Ball", 'code' => "3DcAM01", 'image' => "images/smallBall1.jpg", 'price' => "35.00", 'description' => " Practice and one day you will be the best!." ), 
- array( 'id' => "2", 'name' => "Chuteira", 'code' => "USB02", 'image' => "images/smallChuteira.jpg", 'price' => "190.00", 'description' => "Be a professional!." ), 
- array( 'id' => "3", 'name' => "Brazil soccer-Short", 'code' => "short03", 'image' => "images/smallShort1.jpg", 'price' => "210.00", 'description' => " Be a winner!! " ));
+$product_array = array(
+	array('id' => "1", 'name' => "Soccer-Ball", 'code' => "3DcAM01", 'image' => "images/smallBall1.jpg", 'price' => "35.00", 'description' => " Practice and one day you will be the best!."),
+	array('id' => "2", 'name' => "Chuteira", 'code' => "USB02", 'image' => "images/smallChuteira.jpg", 'price' => "190.00", 'description' => "Be a professional!."),
+	array('id' => "3", 'name' => "Brazil soccer-Short", 'code' => "short03", 'image' => "images/smallShort1.jpg", 'price' => "210.00", 'description' => " Be a winner!! ")
+);
 
 
- $action = filter_input(INPUT_POST, 'action');
-if ($action == NULL){
- $action = filter_input(INPUT_GET, 'action');
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+	$action = filter_input(INPUT_GET, 'action');
 }
-switch($action) {
+switch ($action) {
 	case "add":
-		if(!empty($_POST["quantity"])) {
-			foreach($product_array as $key) {
-				if($key["code"] == $_GET["code"]) { 
+		//if (!empty($_POST["quantity"])) {
+			foreach ($product_array as $key) {
+				if ($key["code"] == $_GET["code"]) {
 					$productByCode = $key;
 				}
-			}
-			
+			//}
 
 
-			$itemArray = array($productByCode["code"]=>array('name'=>$productByCode["name"], 'code'=>$productByCode["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode["price"]));
-		
-			
+
+			$itemArray = array($productByCode["code"] => array('name' => $productByCode["name"], 'code' => $productByCode["code"], 'quantity' => $_POST["quantity"], 'price' => $productByCode["price"]));
 
 
-			if(!empty($_SESSION["cart_item"])) {
-				if(in_array($product_array[0]["code"],array_keys($_SESSION["cart_item"]))) {
-					foreach($_SESSION["cart_item"] as $k => $v) {
-							if($product_array[0]["code"] == $k) {
-								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
-									$_SESSION["cart_item"][$k]["quantity"] = 0;
-								}
-								$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+
+
+			if (!empty($_SESSION["cart_item"])) {
+				if (in_array($product_array[0]["code"], array_keys($_SESSION["cart_item"]))) {
+					foreach ($_SESSION["cart_item"] as $k => $v) {
+						if ($product_array[0]["code"] == $k) {
+							if (empty($_SESSION["cart_item"][$k]["quantity"])) {
+								$_SESSION["cart_item"][$k]["quantity"] = 0;
 							}
+							$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+						}
 					}
 				} else {
-					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray);
 				}
 			} else {
 				$_SESSION["cart_item"] = $itemArray;
 			}
 		}
 		include 'myBrowser.php';
-	break;
-	
+		break;
+
 	case "remove":
-		if(!empty($_SESSION["cart_item"])) {
-			foreach($_SESSION["cart_item"] as $k => $v) {
-					if($_GET["code"] == $k)
-						unset($_SESSION["cart_item"][$k]);				
-					if(empty($_SESSION["cart_item"]))
-						unset($_SESSION["cart_item"]);
+		if (!empty($_SESSION["cart_item"])) {
+			foreach ($_SESSION["cart_item"] as $k => $v) {
+				if ($_GET["code"] == $k)
+					unset($_SESSION["cart_item"][$k]);
+				if (empty($_SESSION["cart_item"]))
+					unset($_SESSION["cart_item"]);
 			}
 		}
 		include 'myBrowser.php';
-	break;
-	
+		break;
+
 	case "details":
-		foreach($product_array as $key) {
-			if($key["code"] == $_GET["code"]) { 
+		foreach ($product_array as $key) {
+			if ($key["code"] == $_GET["code"]) {
 				$itemArray = $key;
 			}
 		}
 		include 'myDetails.php';
-	break;
+		break;
 	case "view":
 		include 'myCart.php';
-	break;
+		break;
 	case "checkout":
 		include 'checkout.php';
-	break;
+		break;
 	case "orderconfirm":
 		$clientName = filter_input(INPUT_POST, 'clientName', FILTER_SANITIZE_STRING);
 		$clientEmail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -80,16 +82,15 @@ switch($action) {
 		$clientCity = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
 		$clientState = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING);
 		$clientZipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_STRING);
-	
+
 		include 'confirmOrder.php';
-	break;
+		break;
 	case "empty":
 		unset($_SESSION["cart_item"]);
 		include 'myBrowser.php';
-	break;	
-	
+		break;
+
 	default:
 		include 'myBrowser.php';
 		break;
-
-	}
+}
