480a924222d9bfae656b589987d854d98f77d32b
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-05 18:31:16 -0600

    week03

--- web/week03/shoppingCart/address.php
+++ web/week03/shoppingCart/address.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thank You</title>
-    <link rel="stylesheet" href="css/week3.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
