fe9fe6f0f4a2bc57f2287d30c32f9381f751eed3
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-07 12:10:25 -0600

    myShoppingCart

--- web/week03/shoppingCart/myCart.php
+++ web/week03/shoppingCart/myCart.php
@@ -12,7 +12,7 @@
 <body>
 	<main>
 		<div id="shopping-cart">
-			<div class="txt-heading">Shopping Cart <a id="btnView" href="index.php?action=browse">Gop Back</a><a id="btnEmpty" href="index.php?action=empty">Empty Cart </a></div>
+			<div class="txt-heading">Shopping Cart <a id="btnView" href="index.php?action=browser">Gop Back</a><a id="btnEmpty" href="index.php?action=empty">Empty Cart </a></div>
 			<?php
 			if (isset($_SESSION["cart_item"])) {
 				$item_total = 0;
@@ -30,11 +30,16 @@
 							foreach ($_SESSION["cart_item"] as $item) {
 								?>
 							<tr>
-								<td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><strong><?php echo $item["name"]; ?></strong></td>
-								<td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><?php echo $item["code"]; ?></td>
-								<td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo $item["quantity"]; ?></td>
-								<td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo "$" . $item["price"]; ?></td>
-								<td style="text-align:center;border-bottom:#F0F0F0 1px solid;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction">Remove Item</a></td>
+								<td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><strong>
+									<?php echo $item["name"]; ?></strong></td>
+								<td style="text-align:left;border-bottom:#F0F0F0 1px solid;">
+								<?php echo $item["code"]; ?></td>
+								<td style="text-align:right;border-bottom:#F0F0F0 1px solid;">
+								<?php echo $item["quantity"]; ?></td>
+								<td style="text-align:right;border-bottom:#F0F0F0 1px solid;">
+								<?php echo "$" . $item["price"]; ?></td>
+								<td style="text-align:center;border-bottom:#F0F0F0 1px solid;">
+								<a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction">Remove Item</a></td>
 							</tr>
 						<?php
 								$item_total += ($item["price"] * $item["quantity"]);
--- web/week03/shoppingCart/myShoppingCart.php
+++ web/week03/shoppingCart/myShoppingCart.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HTML Form</title>
-    <link rel="stylesheet" href="../css/week3.css">
+    <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     
     <script src="js/week3.js"></script>
