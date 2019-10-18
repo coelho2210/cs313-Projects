b6279d58770f5e4aafadba0ed742a0c623121d1d
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-18 17:29:21 -0600

    new changes1

--- web/week05/navbar.php
+++ web/week05/navbar.php
@@ -1,6 +1,6 @@
 <h1> My Rating Park</h1>
 
 <ul class="nav">
-  <li class="nav"><a href="home.php" class="nav">Home</a></li>
+  <li class="nav"><a href="home.php" class="nav">About this Project</a></li>
   <li class="nav"><a href="index.php" class="nav">My Browser</a></li>
 </ul>
