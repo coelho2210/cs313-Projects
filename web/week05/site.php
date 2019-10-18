7225c8af105d32b80c2735d27c9cacc44fd167ac
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-17 20:13:27 -0600

    changes

--- web/week05/site.php
+++ web/week05/site.php
@@ -44,7 +44,7 @@ if (isset($name) and isset($description) and isset($rating) and !isset($_SESSION
 	<?php 
 	include 'navbar.php';
 	
-	$stmt = $db->prepare("SELECT url FROM picture WHERE site_id=:siteId");
+	$stmt = $db->prepare("SELECT url FROM picture WHERE park_id=:siteId");
 	$stmt->bindValue(':siteId', $siteId, PDO::PARAM_STR);
 	$stmt->execute();
 	$stmt->bindColumn(1, $url);
