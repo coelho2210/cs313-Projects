2ac1239171cfc760b1d34cb5713d882032b986bc
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-19 11:14:09 -0700

    new changes

--- web/week05/index.php
+++ web/week05/index.php
@@ -17,12 +17,12 @@
 		echo "<h3>" . $state_row["name"] . "</h3>";
 		$stateId = $state_row["id"];
 			
-		foreach ($db->query("SELECT * FROM city WHERE state_id=$stateId") as $county_row) {
-			echo "<h4>" . $county_row["name"] . "</h4>";
+		foreach ($db->query("SELECT * FROM city WHERE state_id=$stateId") as $city_row) {
+			echo "<h4>" . $city_row["name"] . "</h4>";
 			echo "<ul>";
-			$countyId = $county_row["id"];
+			$cityId = $ciy_row["id"];
 				
-			foreach ($db->query("SELECT * FROM park WHERE city_id=$countyId") as $site_row) {
+			foreach ($db->query("SELECT * FROM park WHERE city_id=$cityId") as $site_row) {
 				$siteId = $site_row["id"];
 				echo "<li><a href='site.php?siteId=$siteId'>" . $site_row["name"] . "</a></li>";
 			}
