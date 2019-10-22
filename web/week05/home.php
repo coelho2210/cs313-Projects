c6f3c11778111f0c0d4efa36defa21ed99e42041
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-21 21:23:38 -0600

    new park.php

--- web/week05/home.php
+++ web/week05/home.php
@@ -5,7 +5,7 @@
 </head>
 
 <body>
-<div id="back">
+<div id="background">
 	<?php include 'navbar.php'; ?>
 	<h2>This Project</h2>
 	<p>This is a web application that allows the user to insert new data, update exintg data, and delete if  necessary. This aplication taught me  how to work with databse and PHP.</p>
