63edd41982ee238bb4355945f583faf844d6a88e
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-23 14:37:35 -0600

    add review

--- web/week05/create_user.php
+++ web/week05/create_user.php
@@ -9,7 +9,7 @@ session_start();
         <meta charset="utf-8">
         <title>Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
 
     </head>
 
