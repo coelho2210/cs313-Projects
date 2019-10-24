c1d263e06d5560bc498947d1d51403da5bc91d99
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-24 15:43:53 -0600

    my changes

--- web/week05/db_access.php
+++ web/week05/db_access.php
@@ -1,19 +1,17 @@
 <?php
+function dbConnect(){
+    $dbUrl = getenv('DATABASE_URL');
 
-$dbUrl = getenv('DATABASE_URL');
+    $dbopts = parse_url($dbUrl);
 
-$dbopts = parse_url($dbUrl);
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"], '/');
 
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 ?>
