c7e35d202ac978c658fbdb4a13196047f5074a52
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-25 12:55:41 -0600

    my changes

--- web/week05/login.php
+++ web/week05/login.php
@@ -51,7 +51,7 @@ if (isset($_POST['user']) && isset($_POST['pass']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>User | Login</title>
 </title>
 <body>
