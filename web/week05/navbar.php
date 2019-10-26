260f1cceea29e79065b93894cf2a2c69fbac18f8
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-26 16:51:48 -0600

    login

--- web/week05/navbar.php
+++ web/week05/navbar.php
@@ -4,4 +4,5 @@
   <li class="nav"><a href="home.php" class="nav">About this Project</a></li>
   <li class="nav"><a href="index.php" class="nav">My Browser</a></li>
   <li class="nav"><a href="login.php" class="nav"> Login</a></li>
+  <li class="nav"><a href="sign_up.php" class="nav"> Sign Up</a></li>
 </ul>
