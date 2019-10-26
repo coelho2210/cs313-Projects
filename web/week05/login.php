5da0208b9e5837907edc3a1ad21fd4170ed4754f
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-26 17:57:29 -0600

    login

--- web/week05/login.php
+++ web/week05/login.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 // check if I have user and password
-if (isset($_POST['user']) && isset($_POST['pass']))
-{
+if (isset($_POST['user']) && isset($_POST['pass'])) {
     //store the user and password
     $username = htmlspecialchars($_POST['user']);
     $password = htmlspecialchars($_POST['pass']);
-    
+
     //catch the database
     require_once("db_access.php");
     $db = get_db();
@@ -15,30 +14,23 @@ if (isset($_POST['user']) && isset($_POST['pass']))
     $stmt = $db->prepare($query);
     $stmt->bindValue(':username', $username);
     $result = $stmt->execute();
-    if ($result)
-    {
-        
+    if ($result) {
+
         $row = $stmt->fetch();
         // get the password from database
         $hashedPassword = $row['password'];
-        
-    
+
+
         //verify if stored password matches password in my database  
-        if (password_verify($password, $hashedPassword))
-        {
+        if (password_verify($password, $hashedPassword)) {
             $_SESSION['username'] = $username;
-            
+
             header("Location: index.php");
             die();
-            
-        }
-        else 
-        {
+        } else {
             $badLogin = true;
         }
-    }
-    else
-    {
+    } else {
         $badLogin = true;
     }
 }
@@ -46,28 +38,38 @@ if (isset($_POST['user']) && isset($_POST['pass']))
 
 <!DOCTYPE html>
 <html lang="en">
-<title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<head>
+    <title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>User | Login</title>
+    </title>
     <link rel="stylesheet" href="css/style.css">
-    <title>User | Login</title>
-</title>
-<header><h1>Login</h1></header> 
+</head>
+<header>
+    <h1>Login</h1>
+</header>
+
 <body>
-    
-    <div id="back" style="margin-left: 10%; margin-top:10%;"> 
-    <?php include 'navbar.php'; ?>  
 
-    <div class="login">
-        <form action='login.php' method='post'>
-        <input type="text"  name="user" placeholder="Username"/><br>
-        <input type="password"  name="pass" placeholder="Password"/><br>
-        <p><?php if ($badlogin) {echo 'Your username or password is incorrect';} ?></p>
-        <input type="submit" value="Login">
-        </form>
+    <div id="back" style="margin-left: 10%; margin-top:10%;">
+        <?php include 'navbar.php'; ?>
 
+        <div class="login">
+            <form action='login.php' method='post'>
+                <input type="text" name="user" placeholder="Username" /><br>
+                <input type="password" name="pass" placeholder="Password" /><br>
+                <p><?php if ($badlogin) {
+                        echo 'Your username or password is incorrect';
+                    } ?></p>
+                <input type="submit" value="Login">
+            </form>
+
+        </div>
     </div>
-</div>
 </body>
+
 </html>
