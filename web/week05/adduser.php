0930d834e631643094c7ff643611b1fd4454e0ea
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-26 19:02:18 -0600

    login

--- web/week05/adduser.php
+++ web/week05/adduser.php
@@ -10,6 +10,9 @@ error_reporting(E_ALL);
     $cemail = htmlspecialchars($_POST['cemail']);
     $fname = htmlspecialchars($_POST['fname']);
     $lname = htmlspecialchars($_POST['lname']);
+
+    var_dump($user, $pass, $cpass, $email, $cemail, $fname, $lname);
+    exit;
     
     if (!isset($user) || $user == "") {
         header("location: sign_up.php");
