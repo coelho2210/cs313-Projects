db4c386406ac0ed5083d3558eaec7e9940010a41
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-26 19:58:13 -0600

    login

--- web/week06/navbar.php
+++
@@ -1,7 +0,0 @@
-<h1> My Rating Park - Webpage</h1>
-
-<ul class="nav">
-  <li class="nav"><a href="home.php" class="nav">About this Project</a></li>
-  <li class="nav"><a href="index.php" class="nav">My Browser</a></li>
-  <li class="nav"><a href="login.php" class="nav">login</a></li>
-</ul>
