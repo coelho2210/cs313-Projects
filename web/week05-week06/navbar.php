2d35d1a8bb3d4fd40c9e231f005fb33af08a404f
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-31 15:31:12 -0600

    navbar

--- web/week05-week06/navbar.php
+++ web/week05-week06/navbar.php
@@ -1,9 +1,23 @@
-<h1> My Rating Park - Webpage</h1>
+<?php
 
-<ul class="nav">
-  <li class="nav"><a href="home.php" class="nav">About this Project</a></li>
-  <li class="nav"><a href="index.php" class="nav">My Browser</a></li>
-  <li class="nav"><a href="login.php" class="nav"> Login</a></li>
-  <li class="nav"><a href="sign_up.php" class="nav"> Sign Up</a></li>
-  <li class="nav"><a href="signOut.php" class="nav"> Sign Out</a></li>
-</ul>
+if (isset($_SESSION['username'])) {
+    echo "<h1> My Rating Park - Webpage</h1>";
+    echo "<ul class='nav'>";
+    echo "<li class='nav><a href='home.php' class='nav'>About this Project</a></li>";
+    echo "<li class='nav'><a href='index.php' class='nav'>My Browser</a></li>";
+    echo "<li class='nav'><a href='signOut.php' class='nav'> Sign Out</a></li>";
+    echo "</ul>";
+} else {
+
+echo "<ul class='nav'>";
+echo"<li class='nav'><a href='login.php' class='nav'> Login</a></li>";
+echo"<li class='nav'><a href='sign_up.php' class='nav'> Sign Up</a></li>";
+echo"</ul>";
+
+}
+?>
+
+
+
+
+
