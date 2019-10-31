017f9c6a72dbddfd8edad97a9bf764461bc99707
Leonardo Silva <coelho_ll@hotmail.com>
2019-10-31 15:32:37 -0600

    navbar

--- web/week05-week06/navbar.php
+++ web/week05-week06/navbar.php
@@ -3,7 +3,7 @@
 if (isset($_SESSION['username'])) {
     echo "<h1> My Rating Park - Webpage</h1>";
     echo "<ul class='nav'>";
-    echo "<li class='nav><a href='home.php' class='nav'>About this Project</a></li>";
+    echo "<li class='nav'><a href='home.php' class='nav'>About this Project</a></li>";
     echo "<li class='nav'><a href='index.php' class='nav'>My Browser</a></li>";
     echo "<li class='nav'><a href='signOut.php' class='nav'> Sign Out</a></li>";
     echo "</ul>";
