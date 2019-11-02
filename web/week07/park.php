28b503160d5f0b13fa57be16300f7c165da7305d
Leonardo Silva <coelho_ll@hotmail.com>
2019-11-02 15:08:02 -0600

    delete

--- web/week07/park.php
+++ web/week07/park.php
@@ -1,9 +1,9 @@
 <?php
 
-//debug coding
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+// debug coding
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 session_start();
 
